762e2ec2ce63a0bf6ac61723670610a84850c9a3
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-27 19:22:22 -0500

    feat: initial commit

---
+++ src/test/java/org/mrpaulwoods/statemachinedemo/StateMachineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrpaulwoods.statemachinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateMachineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
